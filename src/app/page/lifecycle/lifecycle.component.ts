95a235e78e64ef6e7c56fc2029adec67d8bda8d6
Alison Wu <alison.wu23@gmail.com>
2025-02-09 13:13:17 +1000

    feat: update lifecycle

--- src/app/page/lifecycle/lifecycle.component.ts
+++ src/app/page/lifecycle/lifecycle.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { TestComponent } from './test/test.component';
+import { DoCheckComponent } from './do-check/do-check.component';
+import { OnChangeIndexComponent } from './on-changes/on-change-index/on-change-index.component';
 
 @Component({
   selector: 'app-lifecycle',
-  imports: [TestComponent],
+  imports: [OnChangeIndexComponent, DoCheckComponent],
   templateUrl: './lifecycle.component.html',
   styleUrl: './lifecycle.component.scss',
 })
 export class LifecycleComponent implements OnInit {
   constructor() {
-    console.log('LifecycleComponent constructor', this.inputValue);
+    // console.log('LifecycleComponent constructor', this.inputValue);
     // - The constructor of a component class is called immediately when the component is instantiated.
     // - This happens before Angular sets up the component's inputs or runs change detection.
     //   However, the constructor doesn't wait for the component's view to be fully rendered.
@@ -29,26 +30,33 @@ export class LifecycleComponent implements OnInit {
     }, 1000);
   }
   ngOnInit(): void {
-    console.log('[LC] - ngOnInit: Component has been initialized');
-    this.startCounter();
+    // console.log('[LC] - ngOnInit: Component has been initialized');
+    // this.startCounter();
     // - While the ngOnInit( lifecycle hook is called after the component has been initialized,
     //   meaning that Angular has finished creating the component & has set up all its inputs.
     // - This is also before the component's view is rendered in the DOM, but it's after Angular has set up the data-bound properties.
   }
   // ngOnInit - Ends
 
-  // ngOnChanges - Begins
-  inputValue: string = 'Initial Value';
-  updateValue() {
-    this.inputValue = 'App Component';
+  // ngDoCheck - Begins
+  // 1. Use the ngOnChanges) to check the limitations.
+  // 2. Refactor it using the ngDoCheck().
+  user = {
+    name: 'UserA',
+  };
+  changeUserName() {
+    this.user.name = 'UserB';
+    // Changing the entire user object would have triggered the change, but not for a particular object property.
+    // With the ngDoCheck) hook a custom change detection for a component can be created
   }
-  // ngOnChanges - Ends
+
+  // ngDoCheck - Ends
 
   // creation phases in order
   // (1) -> `constructor()`: constructor() gets called & object is created for the component. [Memory initialization] <-> [Browser DOM] the component is not completely rendered.
   // (2) -> `ngOnInit()`: ngOnInit() waits for the component to get rendered. it's a preferred phase to fetch api.
-  // ngOnChanges
-  // ngDoCheck
+  // (3) -> `ngOnChanges()`: responded to the changes made with the data bound input property.
+  // (4) -> `ngDoCheck()`: is used for performing a custom change detection & responding to the changes in a component.
   // - (Main used in the child components & DOM manipulation)
   // | ngAfterContentInit
   // | ngAfterContentChecked
