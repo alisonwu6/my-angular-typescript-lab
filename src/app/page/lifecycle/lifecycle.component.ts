2159f925b1844bf7424db4011799eb694e3605ad
Alison Wu <alison.wu23@gmail.com>
2025-02-09 11:48:53 +1000

    feat: lifecycle - onInit

--- src/app/page/lifecycle/lifecycle.component.ts
+++ src/app/page/lifecycle/lifecycle.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TestComponent } from './test/test.component';
 
 @Component({
@@ -7,24 +7,50 @@ import { TestComponent } from './test/test.component';
   templateUrl: './lifecycle.component.html',
   styleUrl: './lifecycle.component.scss',
 })
-export class LifecycleComponent {
-  inputValue: string = 'Initial Value';
-
+export class LifecycleComponent implements OnInit {
   constructor() {
     console.log('LifecycleComponent constructor', this.inputValue);
+    // - The constructor of a component class is called immediately when the component is instantiated.
+    // - This happens before Angular sets up the component's inputs or runs change detection.
+    //   However, the constructor doesn't wait for the component's view to be fully rendered.
   }
 
+  // ngOnInit - Begins
+  count: number = 0;
+  counterInterval: any;
+  startCounter() {
+    this.counterInterval = setInterval(() => {
+      if (this.count < 3) {
+        console.log(this.count++, this.counterInterval);
+      } else {
+        clearInterval(this.counterInterval);
+        console.log('this.counterInterval', this.counterInterval);
+      }
+    }, 1000);
+  }
+  ngOnInit(): void {
+    console.log('[LC] - ngOnInit: Component has been initialized');
+    this.startCounter();
+    // - While the ngOnInit( lifecycle hook is called after the component has been initialized,
+    //   meaning that Angular has finished creating the component & has set up all its inputs.
+    // - This is also before the component's view is rendered in the DOM, but it's after Angular has set up the data-bound properties.
+  }
+  // ngOnInit - Ends
+
+  // ngOnChanges - Begins
+  inputValue: string = 'Initial Value';
   updateValue() {
     this.inputValue = 'App Component';
   }
+  // ngOnChanges - Ends
 
   // creation phases in order
-  // constructor
+  // (1) -> `constructor()`: constructor() gets called & object is created for the component. [Memory initialization] <-> [Browser DOM] the component is not completely rendered.
+  // (2) -> `ngOnInit()`: ngOnInit() waits for the component to get rendered. it's a preferred phase to fetch api.
   // ngOnChanges
-  // ngOnInit
   // ngDoCheck
   // - (Main used in the child components & DOM manipulation)
-  // | ngAfterContentInit    
+  // | ngAfterContentInit
   // | ngAfterContentChecked
   // | ngAfterViewInit
   // - ngAfterViewChecked
