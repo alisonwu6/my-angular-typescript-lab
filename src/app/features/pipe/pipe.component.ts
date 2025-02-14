2e732b8717720ac1c1be6ab3921205aa11a08052
Alison Wu <alison.wu23@gmail.com>
2025-02-14 15:38:55 +1000

    feat: Pipes - 84. Understanding Slice Pipe

--- src/app/features/pipe/pipe.component.ts
+++ src/app/features/pipe/pipe.component.ts
@@ -11,13 +11,16 @@ export class PipeComponent {
   title: string = 'Example of pipe expression';
   showDt: Date = new Date();
 
-  //  Task - Implementing the built-in pipes
+  //  83. Task - Implementing the built-in pipes
   employees = [
     { name: 'userA', state: 'california', salary: 50000 },
     { name: 'userB', state: 'new york', salary: 50000 },
     { name: 'userC', state: 'texas', salary: 50000 },
     { name: 'userD', state: 'pennsylvania', salary: 50000 },
   ];
+
+  // 84. Understanding Slice Pipe
+  sliceArr = ['ItemA', 'ItemB', 'ItemC', 'ItemD'];
 }
 
 /**
