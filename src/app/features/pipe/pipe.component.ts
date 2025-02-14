313e67c272b84fb7290b550292cdac0b8a6e7028
Alison Wu <alison.wu23@gmail.com>
2025-02-14 15:23:05 +1000

    feat: Pipes - 83. Task - Implementing the built-in pipes

--- src/app/features/pipe/pipe.component.ts
+++ src/app/features/pipe/pipe.component.ts
@@ -10,6 +10,14 @@ import { Component } from '@angular/core';
 export class PipeComponent {
   title: string = 'Example of pipe expression';
   showDt: Date = new Date();
+
+  //  Task - Implementing the built-in pipes
+  employees = [
+    { name: 'userA', state: 'california', salary: 50000 },
+    { name: 'userB', state: 'new york', salary: 50000 },
+    { name: 'userC', state: 'texas', salary: 50000 },
+    { name: 'userD', state: 'pennsylvania', salary: 50000 },
+  ];
 }
 
 /**
