a9a48009a3f15333af431a6b75439daad1aa267c
Alison Wu <alison.wu23@gmail.com>
2025-02-14 12:58:41 +1000

    feat: 79. Method Decorators - Binding Methods To Events With @HostListener

--- src/app/page/decorator/host-binding-child.directive.ts
+++ src/app/page/decorator/host-binding-child.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, OnInit } from '@angular/core';
+import { Directive, HostBinding, HostListener, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appHostBindingChild]',
@@ -7,6 +7,32 @@ export class HostBindingChildDirective implements OnInit {
   // It is mostly used for styling directives or attribute manipulation. Third party integrations and animating the directives.
   @HostBinding('style.backgroundColor') bgColor?: string;
   @HostBinding('style.color') textColor?: string;
+
+  colorArray = ['seagreen', 'yellow', 'blue', 'red'];
+
+  private setRandomColor() {
+    const colorPick = Math.floor(Math.random() * this.colorArray.length);
+
+    if (this.bgColor !== this.colorArray[colorPick]) {
+      this.bgColor = this.colorArray[colorPick];
+      this.textColor = '#ccc';
+    } else {
+      this.setRandomColor();
+    }
+  }
+
+  // any dom event like click, focus, keyup, keydown, blur etc.
+  @HostListener('click') onToggle() {
+    // if (this.bgColor === '#f2aa4cff') {
+    //   this.bgColor = '#101820ff';
+    //   this.textColor = '#f2aa4cff';
+    // } else {
+    //   this.bgColor = '#f2aa4cff';
+    //   this.textColor = '#101820ff';
+    // }
+
+    this.setRandomColor();
+  }
 
   ngOnInit(): void {
     this.bgColor = '#f2aa4cff';
